d271a9f44a074d725a4484b0bf0e643f9f36193d
uunnnnna <126747689+uunnnnna@users.noreply.github.com>
2024-01-03 22:58:42 +0900

    Create 알람시계.java

---
+++ 백준/Bronze/2884. 알람시계/알람시계.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int H = sc.nextInt();
+		int M = sc.nextInt();
+		
+		if(H == 0) {
+			if(M < 45) {
+				H = 23;
+				M = M+15;
+			}
+			else if(M == 45) M = 0;
+			else M = M-45;
+		}
+		else if( H != 0) {
+			if(M < 45) {
+				H = H-1;
+				M = M+15;
+			}
+			else if(M == 45) M = 0;
+			else M = M-45;
+		}
+		System.out.println(H + " " + M);
+	}
+}
